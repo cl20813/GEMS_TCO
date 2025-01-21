d9d4333e2c35f10ff7e0f137e38bb707236cd146
Joonwon Lee <joonwon.lee22@gmail.com>
2025-01-20 20:44:39 -0500

    dd

--- GEMS_TCO/setup.py
+++ GEMS_TCO/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GEMS_TCO',                      # Name of your package
-    version='0.2',                    # Version of your package
+    version='0.1',                    # Version of your package
     packages=find_packages(),         # Automatically find all packages and modules
     install_requires=[                # List any dependencies here
         'numpy>=1.22,<2.1',
