09e1712119b0fe9eb908f1c3a2863743704d9c3a
Joonwon_Lee <joonwon.lee22@gmail.com>
2025-05-03 22:32:44 -0400

    uu

---
+++ src/GEMS_TCO/configuration.py
@@ -0,0 +1,22 @@
+# mac
+mac_data_load_path = "/Users/joonwonlee/Documents/GEMS_DATA"
+
+mac_estimates_day_path = "/Users/joonwonlee/Documents/GEMS_TCO-1/Exercises/st_model/estimates/save/day"
+
+mac_full_day_v05_pickle = "full_estimates_1250_july24.pkl"
+mac_full_day_v05_csv = "full_estimates_1250_july24.csv"
+
+# amarel
+amarel_data_load_path = "/home/jl2815/tco/data/pickle_data"
+
+amarel_estimates_day_path = "/home/jl2815/tco/exercise_output/estimates/day/saved/"
+amarel_full_day_v05_pickle = "full_day_v(0.5)_1250_july24.pkl"
+
+amarel_estimate_output_path = "/home/jl2815/tco/exercise_output/estimates/day/"
+
+# window
+window_data_load_path = "C:\\Users\\joonw\\tco\\Extracted_data"
+
+window_estimates_day_path = "C:\\Users\\joonw\\tco\\GEMS_TCO-2\\Exercises\\st_model\\estimates\\"   
+window_full_day_v05_pickle  = "full_estimates_1250_july24.pkl"
+
